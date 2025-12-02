e4cc4821515714306782ceb57bff798ffa32825c
admin <admin@admins-MacBook-Pro.local>
2025-12-02 16:41:43 +0700

    blur placeholder

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -24,7 +24,7 @@ export default function HeroSection() {
           src="/img/bg-hero.jpg"
           alt=""
           fill
-          className="absolute inset-0 object-cover z-0"
+          className="absolute inset-0 object-cover -z-10"
           priority
           quality={80}
         />
